7756f4d7e532cedeba06a179320aded0cadabeb6
USER1 <USER1@USER7-PC>
2019-10-28 17:08:19 +0900

    수정

--- VendingMachineVer1/src/com/hk/vmv3/Main.java
+++ VendingMachineVer1/src/com/hk/vmv3/Main.java
@@ -16,28 +16,22 @@ public class Main {
 		Customer cust = new Customer(name, money);
 		// 확인하고 싶으면 toString 오버라이딩 활용하여 출력해본다.
 		System.out.println();
-		
+
 		while (money > 0) {
 			vm.showDrinks();
 
 			System.out.print("\n구매할 음료수 번호를 입력하세요 : ");
 			int idx = sc.nextInt();
 
-			vm.sales(idx, cust);
-			System.out.println();
-			
-			vm.showDrinks();
-			cust.showState();
-
-			System.out.print("계속 하시겠습니까?('y' or 'n') : ");
-			String answer = sc.next();
-			if (answer == "y" || answer == "Y") {
-				vm.showDrinks();
-			} else if(answer == "n" || answer == "N"){
-				System.out.println("종료합니다.");
+			if (idx == 0) {
 				break;
 			}
+			vm.sales(idx, cust);
+			System.out.println(cust);
 		}
+		vm.showDrinks();
+		cust.showState();
+
 		sc.close();
 	}
 }
